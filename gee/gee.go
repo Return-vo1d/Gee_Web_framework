670ae96b495875d16d706293bbe70abb56592e1a
Return-vo1d <returnvoid.1926@gmail.com>
2022-05-27 19:04:06 +0800

    group

--- gee/gee.go
+++ gee/gee.go
@@ -6,29 +6,57 @@ import (
 )
 
 type HandlerFunc func(c *Context)
+type (
+	Engine struct {
+		router *router
+		*RouterGroup
+		groups []*RouterGroup //store all groups
+	}
 
-type Engine struct {
-	router *router
-}
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc //support middleware
+		parent      *RouterGroup  //support nesting
+		engine      *Engine       //all groups share a Engine instance
+	}
+)
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+} //添加分组
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
 	log.Printf("Route %4s - %s", method, pattern)
-	engine.router.addRoute(method, pattern, handler)
+	group.engine.router.addRoute(method, pattern, handler)
 } //添加路由
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 } //添加GET请求
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 } //添加POST请求
+
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 } //启动一个http服务
+
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	/*
 		key := req.Method + "-" + req.URL.Path
